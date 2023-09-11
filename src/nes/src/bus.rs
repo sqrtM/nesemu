71342221f22981495c3e2d4538a73ad81dd2419f
Mason Pike <mpike@flyamelia.com>
2023-09-11 09:55:55 +0200

    before test refactoring

--- src/nes/src/bus.rs
+++ src/nes/src/bus.rs
@@ -1,7 +1,9 @@
+use std::fs;
+
 use nesemu_core::{Read, Write};
 
 pub struct Bus {
-    pub(crate) ram: [u8; 64 * 1024],
+    pub(crate) ram: [u8; 0xFFFF],
 }
 
 impl Write for Bus {
@@ -18,41 +20,17 @@ impl Read for Bus {
 
 impl Default for Bus {
     fn default() -> Self {
-        let mut ram = [0u8; 64 * 1024];
-        ram[0 + 0x8000] = 0xA2;
-        ram[1 + 0x8000] = 0x0A;
-        ram[2 + 0x8000] = 0x8E;
-        ram[3 + 0x8000] = 0x00;
-        ram[4 + 0x8000] = 0x00;
-        ram[5 + 0x8000] = 0xA2;
-        ram[6 + 0x8000] = 0x03;
-        ram[7 + 0x8000] = 0x8E;
-
-        ram[8 + 0x8000] = 0x01;
-        ram[9 + 0x8000] = 0x00;
-        ram[10 + 0x8000] = 0xAC;
-        ram[11 + 0x8000] = 0x00;
-        ram[12 + 0x8000] = 0x00;
-        ram[13 + 0x8000] = 0xA9;
-        ram[14 + 0x8000] = 0x00;
-        ram[15 + 0x8000] = 0x18;
-
-        ram[16 + 0x8000] = 0x6D;
-        ram[17 + 0x8000] = 0x01;
-        ram[18 + 0x8000] = 0x00;
-        ram[19 + 0x8000] = 0x88;
-        ram[20 + 0x8000] = 0xD0;
-        ram[21 + 0x8000] = 0xFA;
-        ram[22 + 0x8000] = 0x8D;
-        ram[23 + 0x8000] = 0x02;
-
-        ram[24 + 0x8000] = 0x00;
-        ram[25 + 0x8000] = 0xEA;
-        ram[26 + 0x8000] = 0xEA;
-        ram[27 + 0x8000] = 0xEA;
-
-        ram[0xFFFC] = 0x00;
-        ram[0xFFFD] = 0x80;
+        let ram = [0u8; 0xFFFF];
         Bus { ram }
+    }
+}
+
+impl Bus {
+    pub(crate) fn load(&mut self) {
+        let contents: Vec<u8> = fs::read("src/nes/tests/roms/nestest.nes")
+            .expect("Should have been able to read the file");
+        for (i, j) in contents.iter().enumerate() {
+            self.ram[i] = j.clone();
+        }
     }
 }
--- src/nes/src/main.rs
+++ src/nes/src/main.rs
@@ -1,47 +1,18 @@
+use bus::Bus;
+use nesemu_cpu::cpu::CPU;
+
 mod bus;
 
-use bus::Bus;
-use nesemu_core::Write;
-use nesemu_cpu::cpu::{
-    StatusFlag::{C, D, I, N, V, Z},
-    CPU,
-};
-
-/// https://youtu.be/8XmxKPJDGU0?t=1692
-
 fn main() {
-    let bus = Bus::default();
+    let mut bus = Bus::default();
+    bus.load();
     let mut cpu: CPU<_> = CPU::default();
 
     cpu.bus = Some(Box::new(bus));
-    cpu.write(1, 16);
-
     cpu.reset();
 
-    let mut i = 0;
-    while i < 128 {
-        let _ = cpu.clock();
-        // if cpu.cycles == 0 {
-        println!(
-            "[ {:?}, {:?}, {:b} ]",
-            cpu.lookup(cpu.opcode).opcode,
-            cpu.lookup(cpu.opcode).addressing_mode,
-            cpu.status
-        );
-        println!(
-            "[ {:X} :: {}, {}, {}, {}, {}]",
-            cpu.pgrm_ctr, cpu.acc_reg, cpu.x_reg, cpu.y_reg, cpu.status, cpu.stk_ptr
-        );
-        println!(
-            "[ N:{} V:{} D:{} I:{} Z:{} C:{}]",
-            cpu.get_flag(N),
-            cpu.get_flag(V),
-            cpu.get_flag(D),
-            cpu.get_flag(I),
-            cpu.get_flag(Z), // error here ?
-            cpu.get_flag(C),
-        );
-        i += 1;
-        // }
+    loop {
+        cpu.clock();
+        println!("{:?} ", cpu.lookup(cpu.opcode));
     }
 }
